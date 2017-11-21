3e636406f517fb685b856fe4079ea2dcca0d813b
wonder2025 <757360513@qq.com>
2017-11-21 22:33:13 +0800

    a

--- presentation.R
+++ presentation.R
@@ -19,11 +19,20 @@ ggplot(Salaries,aes(x=rank,fill=sex))+
 
 ggplot(Salaries,aes(x=rank,fill=sex))+
   geom_bar(position = "fill")+
-  labs(title='position="fill"')
+  labs(title='position="fill"',y="proportion")
 
 ggplot(Salaries,aes(x=rank,fill=sex))+
   geom_bar(position = "identity",alpha=0.3)+
   labs(title='position="identity"')
+
+#构建不同于mtcars的数据集mtcars.c
+mtcars.c <- transform(mtcars, mpg = mpg^2)
+ggplot()+
+  geom_point(aes(x = hp, y = mpg), data = mtcars, color = "red") + 
+  geom_point(aes(x = mtcars$hp, y = mtcars$disp), color = "green")+ 
+  #选用向量数据
+  geom_point(aes(x = hp, y= mpg), data = mtcars.c, color = "blue") 
+#选用不同的数据集
 
 
 
