1f2a9554ce5ba3426d20dc5b24d66f77a91ebe33
liting <757360513@qq.com>
2017-11-23 21:00:30 +0800

    1

--- RServerTest1.R
+++ RServerTest1.R
@@ -9,8 +9,31 @@ rxGetInfo(mortDataXDF)
 
 rxImport(inData = inDataFile,outFile = mortDataXDF,rowsPerRead = 200,overwrite = TRUE)
 
-rxGetInfo(mortDataXDF,getVarInfo = TRUE,getBlockSizes = 100)
+rxGetInfo(mortDataXDF,getVarInfo = TRUE,getBlockSizes = TRUE)
 
 library(parallel)
 #核数
 detectCores()
+
+ls("package:datasets")
+
+nrow(mortData)
+ncol(mortData)
+names(mortData)
+head(mortData, n = 3)
+rxGetInfo(mortData, getVarInfo = TRUE, numRows=3)
+
+mortDataNew <- rxDataStep(
+  # Specify the input data set
+  inData = mortData,
+  # Put in a placeholder for an output file
+  outFile = outFile2,
+  # Specify any variables to keep or drop
+  varsToDrop = c("year"),
+  # Specify rows to select
+  rowSelection = creditScore < 850,
+  # Specify a list of new variables to create
+  transforms = list(
+    catDebt = cut(ccDebt, breaks = c(0, 6500, 13000),
+                  labels = c("Low Debt", "High Debt")),
+    lowScore = creditScore < 625))
