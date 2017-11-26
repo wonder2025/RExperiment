d17de9ba07ee9fcfba79bf182b06feb295d989c5
wonder2025 <757360513@qq.com>
2017-11-26 23:44:37 +0800

    f

--- yalefaces_PCA_1.R
+++ yalefaces_PCA_1.R
@@ -1,8 +1,7 @@
 library(R.matlab)
 library(psych)
 #path <- system.file("mat-files", package="R.matlab")
-path<-"F:/R/yale人脸origin数据录入/Yale_15cl_1024f_165r_1stCLabel_20160714034545_561caca94011413bbf6a2b59839ad774/Yale_15cl_1024f_165r_1stCLabel"
-pathname <- file.path(path, "Yale_32x32.mat")
+pathname <- file.path(getwd(), "Yale_32x32.mat")
 data <- readMat(pathname)
 fea<-data$fea
 gnd<-data$gnd
@@ -10,25 +9,84 @@ dim(fea)
 dim(gnd)
 #画图
 aa<-fea[23,]
-image(aa)
+typeof(aa)
 im<-matrix(aa,32,32)
 image(im)
 
 
 #只取前面33张脸做PCA
 new_data<-fea[1:33,]
-new_data.scale<-scale(new_data)
-fa.parallel(new_data.scale,fa="pc",n.iter = 100,show.legend =TRUE,main="Scree plot with parellel analysis")
-pc<-principal(new_data.scale,nfactors = 3,rotate="none",sorce="true")
+#归一化
+#new_data.scale<-scale(new_data)
+new_data.scale<-new_data
+dim(new_data.scale)#33*1024
+fa.parallel(new_data.scale,fa="pc",n.iter = 100,
+            show.legend =TRUE,main="Scree plot with parellel analysis")
+abline(h=1)
+pc<-principal(new_data.scale,nfactors = 7 ,rotate="none",sorce="true")
 pc
+#主成分旋转,结果表明不用旋转
+#pc<-principal(new_data.scale,nfactors = 7 ,rotate="varimax")
+#pc
 #kmeans
-fit.km<-kmeans(pc$scores,3,nstart = 100)
-gnd<-gnd[1:33,]
-ct.km<-table(gnd,fit.km$cluster)
-ct.km
-library(flexclust)
-randIndex1<-randIndex(ct.km,correct = FALSE)#0.161
-randIndex1
+dim(pc$scores)#33*7
+if(!require(NbClust)){
+  install.packages("NbClust")
+}else{
+  require(NbClust)
+}
 library(caret)
-covtypeConfusionMatrix <- confusionMatrix(fit.km$cluster, gnd)
+install.packages("clv")
+library(clv)
+#得到分类标签
+gnd1<-gnd[1:33,]
+#选择聚类中心点
+nc<-NbClust(pc$scores,min.nc = 2,max.nc = 10,method = "kmeans")
+table(nc$Best.n[1,])
+
+#聚类函数
+fit.km<-kmeans(pc$scores,3,nstart = 13)
+fitted(fit.km)
+#remove(gnd)
+ct.km<-table(gnd1,fit.km$cluster)
+ct.km
+#library(flexclust)
+#randIndex1<-randIndex(ct.km,correct = FALSE)#0.161
+#randIndex1
+
+set.seed(1234)
+a<-NULL
+res<-NULL
+rand_inds<-NULL
+len<-c(1:30)
+for(j in len){
+  fit.km<-kmeans(pc$scores,6,nstart =j)
+  #混淆矩阵
+ # covtypeConfusionMatrix <- confusionMatrix(fit.km$cluster, gnd1)
+
+  #accuracy
+ # a[j]<-  covtypeConfusionMatrix$overall[1]
+  res <-cbind(res,fit.km$cluster)
+  #兰德系数
+  std <- std.ext(fit.km$cluster,gnd1)
+  rand_inds[j] <- clv.Rand(std)   #Rand Index
+    
+}
+
+max(rand_inds)
+
+plot(len,a)
+#准确率最大值时j的值
+index<-which(a==max(a), arr.ind = TRUE)
+index<-which(rand_inds==max(rand_inds), arr.ind = TRUE)
+plot(len,rand_inds)
+max(rand_inds)
+covtypeConfusionMatrix <- confusionMatrix(res[,6], gnd1)
 covtypeConfusionMatrix
+covtypeConfusionMatrix$overall[1]
+
+for(i in res) {
+  std <- std.ext(res[,3],gnd1)
+  rand_inds[j] <- clv.Rand(std)   #Rand Index
+
+}
