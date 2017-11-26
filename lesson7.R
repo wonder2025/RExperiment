2d5c91a89135284e818798d1ede649d8886cd14a
wonder2025 <757360513@qq.com>
2017-11-26 19:55:37 +0800

    a

--- lesson7.R
+++ lesson7.R
@@ -11,3 +11,20 @@ pc$score
 
 
 #######################考试#######################
+if(!require(psych)){
+  install.packages("psych")
+}
+require(psych)
+
+class(Harman23.cor)
+dim(Harman23.cor$center)
+fa.parallel(Harman23.cor$cov,n.obs = 302,fa="pc",n.iter = 100,
+            show.legend=TRUE,main ="scree plot with parellel analysis")
+abline(h=1,lwd=1,col="grey")#添加一条水平直线y=1，线宽为4
+
+#14-2
+pc<-principal(Harman23.cor$cov,nfactors = 2,rotate = "none")
+pc
+#14-3
+pc<-principal(Harman23.cor$cov,nfactors = 2,rotate = "varimax")
+pc
